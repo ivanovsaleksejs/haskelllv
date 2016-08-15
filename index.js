378abe42b058d549932e2c6eb4c5fe1325f18835
Aleksejs Ivanovs <ivanovs.aleksejs@gmail.com>
2016-08-15 11:30:45 +0300

    CSS changes; New slides

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var currentSlide = null;
 
+var slideNumber = parseInt(location.hash.slice(1)) || 1;
+
 var addClass = function(e, name) {
     e.className += " " + name
 }
@@ -12,21 +14,28 @@ var slideStep = function(dir) {
     if (next != null) {
         removeClass(currentSlide, "active")
         removeClass(currentSlide, "left")
+
         dir && addClass(currentSlide, "rightside")
         dir || addClass(currentSlide, "leftside")
+
         currentSlide = next
+
         dir || removeClass(currentSlide, "rightside")
         dir && removeClass(currentSlide, "leftside")
+
         addClass(currentSlide, "active" + (dir ? "" : " left"))
+
+        slideNumber += dir ? 1 : -1
+        location.hash = slideNumber
     }
 }
 
 var prevSlide = function(){
-    slideStep(false)    
+    slideStep(false)
 }
 
 var nextSlide = function(){
-    slideStep(true)    
+    slideStep(true)
 }
 
 var displayTooltip = function(event) {
@@ -48,11 +57,25 @@ window.onload = function() {
     document.getElementById("next").addEventListener("click", nextSlide, false)
 
     var tooltips = document.getElementsByClassName("def")
+
     for (var i = 0; i < tooltips.length; i++) {
         var e = tooltips[i]
         e.addEventListener("mouseover", displayTooltip)
         e.addEventListener("mouseout", hideTooltip)
     }
+
+    var slides = document.getElementsByClassName("slide")
+
+    if (slideNumber > 1) {
+        for (var i = 0; i < slideNumber - 2; i++) {
+            addClass(slides[i], "rightside")
+        }
+    }
+    if (typeof slides[slideNumber] !== "undefined") {
+        addClass(slides[i], "leftside")
+    }
+
+    addClass(slides[slideNumber-1], "active")
 
     currentSlide = document.getElementsByClassName("slide active")[0]
 
