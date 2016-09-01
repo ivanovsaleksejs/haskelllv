7c645f225d861adcdf41b8378b4b84e57e71a27c
Aleksejs Ivanovs <ivanovs.aleksejs@gmail.com>
2016-09-01 12:40:09 +0300

    GA code

--- index.js
+++ index.js
@@ -185,5 +185,22 @@ window.onload = function() {
 
     summary.addEventListener("click", changeSlide)
 
-    document.addEventListener("click", closeSummary)
+    document.addEventListener("click", closeSummary);
+
+    (function(i,s,o,g,r,a,m){
+        i['GoogleAnalyticsObject']=r;
+        i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)
+        },
+        i[r].l=1*new Date();
+        a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];
+        a.async=1;
+        a.src=g;
+        m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-83556742-1', 'auto');
+    ga('send', 'pageview');
+
 }
