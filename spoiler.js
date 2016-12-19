edb916bb0d5a1a8910b475d3d7de496e72a353e7
Aleksejs Ivanovs <ivanovs.aleksejs@gmail.com>
2016-12-19 20:42:18 +0200

    Added code highlight

--- spoiler.js
+++ spoiler.js
@@ -1,6 +1,11 @@
 var triggerSpoiler = function(e) {
     var elem = e.target
+    if (elem.tagName != "PRE") {
+        elem = elem.parentNode
+    }
+
     hasClass(elem, "revealed") ? removeClass(elem, "revealed") : addClass(elem, "revealed")
+    hasClass(elem, "hidden") ? removeClass(elem, "hidden") : addClass(elem, "hidden")
 }
 
 window.addEventListener("load", function() {
@@ -8,6 +13,12 @@ window.addEventListener("load", function() {
 
     for (var i = 0; i < spoilers.length; i++) {
         var e = spoilers[i]
-        e.addEventListener("click", triggerSpoiler)
+        e.firstChild.addEventListener("click", triggerSpoiler)
+    }
+
+    codes = document.getElementsByTagName('code')
+
+    for (var e=0; e<codes.length; e++) {
+        hljs.highlightBlock(codes[e]);
     }
 })
